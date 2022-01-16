f376fcc299d0f03b5f2b9f08cc1f2e0fbe6a00b9
Sheka799 <sheka1991_91@mail.ru>
2022-01-16 21:48:44 +0300

    Блок 4: домашнее задание № 5

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,70 @@
 <template>
-  <section class="catalog">
+
+  <main class="content container">
+    <div class="content__top content__top--catalog">
+      <h1 class="content__title">
+        Каталог
+      </h1>
+      <span class="content__info">
+        152 товара
+      </span>
+    </div>
+
+    <div class="content__catalog">
+      <ProductFilter :price-from.sync="filterPriceFrom" :price-to.sync="filterPriceTo" :category-id.sync="filterCategoryId" :colors-id.sync="filterColorsId" />
+      <section class="catalog">
         <ProductList :products="products" />
         <BasePagination v-model="page" :count="countProducts" :per-page="productsPerPage" />
       </section>
+    </div>
+  </main>
 </template>
 
 <script>
 import BasePagination from './components/BasePagination.vue';
 import ProductList from './components/ProductList.vue';
+import ProductFilter from './components/ProductFilter.vue';
 import products from './data/products';
 
 export default {
-  components: { ProductList, BasePagination },
+  components: { ProductList, BasePagination, ProductFilter },
   name: 'App',
   data() {
     return {
+      filterPriceFrom: 0,
+      filterPriceTo: 0,
+      filterCategoryId: 0,
+      filterColorsId: 0,
       page: 1,
       productsPerPage: 3,
     };
   },
   computed: {
+    filteredProducts() {
+      let filteredProducts = products;
+
+      if (this.filterPriceFrom > 0) {
+        filteredProducts = filteredProducts.filter((product) => product.price > this.filterPriceFrom);
+      }
+
+      if (this.filterPriceTo > 0) {
+        filteredProducts = filteredProducts.filter((product) => product.price < this.filterPriceTo);
+      }
+
+      if (this.filterCategoryId) {
+        filteredProducts = filteredProducts.filter((product) => product.categoryId === this.filterCategoryId);
+      }
+
+      if (this.filterColorsId) {
+        filteredProducts = filteredProducts.filter((product) => product.colorsId === this.filterColorsId);
+      }
+
+      return filteredProducts;
+    },
     products() {
       const offset = (this.page - 1) * this.productsPerPage;
 
-      return products.slice(offset, offset + this.productsPerPage);
+      return this.filteredProducts.slice(offset, offset + this.productsPerPage);
     },
     countProducts() {
       return products.length;
---
+++ src/data/colors.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    color: '#73B6EA'
+  },
+  {
+    id: 2,
+    color: '#FFBE15'
+  },
+  {
+    id: 3,
+    color: '#FFF'
+  },
+  {
+    id: 4,
+    color: '#000'
+  }
+];
